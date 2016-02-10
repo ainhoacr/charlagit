0d29f1ac3670cf87cd6cbead6d61f0f48f8fd899
Ainhoa Calviño <ainhoa.c.rodriguez@gmail.com>
2016-02-10 19:36:37 +0100

    añadido el código de la pila en python

---
+++ stack.py
@@ -0,0 +1,22 @@
+class Stack:
+    """Implementacion de unha pila facendo uso de obxectos """
+
+    def __init__(self):
+        self.items = []
+
+    def vacia(self):
+        return self.items == []
+
+    def apilar(self, item):
+        self.items.append(item)
+
+    def cima(self):
+        if self.vacia():
+            return []
+        return self.items[-1]
+
+    def desapilar(self):
+        del self.items[-1]
+
+    def tam(self):
+        return len(self.items)
